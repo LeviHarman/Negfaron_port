47a93af29a669f072a8330a9d9e8855afc4f9359
Levi Harman <serpentinexcubed@gmail.com>
2012-05-26 05:19:24 -0500

    added serperate getters for each element in set_specific_values method.

--- hero.cpp
+++ hero.cpp
@@ -12,6 +12,7 @@ class Hero: public Creature {
   int exp_this_level;
   int exp_to_next_level;
   public:
+    //set_stats:str,dex,con,inte,wis,cha
     void Hero::set_stats (int new_str, int new_dex, int new_con, int new_inte, int new_wis, int new_cha) {
       str = new_str;
       dex = new_dex;
@@ -20,7 +21,7 @@ class Hero: public Creature {
       wis = new_wis;
       cha = new_cha;
     }
-    
+    //set_specific_values:level,weapon,gold,experience,exp_this_level,exp_to_next_level
     void Hero::set_specific_values (int new_level, int new_weapon, int new_gold, int new_experience, int new_exp_this_level, int new_exp_to_next_level) {
       level = new_level;
       weapon = new_weapon;
@@ -37,6 +38,13 @@ class Hero: public Creature {
   void set_wis(int a){ wis = a; }
   void set_cha(int a){ cha = a; }
   
+  void set_level(int a){ level = a; }
+  void set_weapon(int a){ weapon = a; }
+  void set_gold(int a){ gold = a; }
+  void set_experience(int a){ experience = a; }
+  void set_exp_this_level(int a){ exp_this_level = a; }
+  void set_exp_to_next_level(int a){ exp_to_next_level = a; }
+  
   int get_str(){ return(str); }
   int get_dex(){ return(dex); }
   int get_con(){ return(con); }
@@ -44,10 +52,11 @@ class Hero: public Creature {
   int get_wis(){ return(wis); }
   int get_cha(){ return(cha); }
   
-  void set_level(int a){ level = a; }
-  void set_weapon(int a){ weapon = a; }
-  void set_gold(int a){ gold = a; }
-  void set_experience(int a){ experience = a; }
-  void set_exp_this_level(int a){ exp_this_level = a; }
-  void set_exp_to_next_level(int a){ exp_to_next_level = a; }
+  int get_level(){ return(level); }
+  int get_weapon(){ return(weapon); }
+  int get_gold(){ return(gold); }
+  int get_experience(){ return(experience); }
+  int get_exp_this_level(){ return(exp_this_level); }
+  int get_exp_to_next_level(){ return(exp_to_next_level); }
+
 };
